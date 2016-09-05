e715321c406b16e9f7266aeb7376cdfa7ee2ee6c
sljivke <sljivovac92@gmail.com>
2016-09-05 09:43:46 +0200

    Proveravamo da li su podaci za logovanje validni

--- xml/XML/src/src/sessionbeans/UserDao.java
+++ xml/XML/src/src/sessionbeans/UserDao.java
@@ -16,33 +16,39 @@ public class UserDao extends GenericDao<User, Integer> {
 		// TODO Auto-generated constructor stub
 	}
 
-public List<User> getAllUsers(){
-      List<User> userList = null;
-      
-      userList = new ArrayList<User>();
-            
-       
-      return userList;
-   }
-
-   private void saveUserList(List<User> userList){
-      try {
-         File file = new File("Users.dat");
-         FileOutputStream fos;
-
-         fos = new FileOutputStream(file);
-
-         ObjectOutputStream oos = new ObjectOutputStream(fos);
-         oos.writeObject(userList);
-         oos.close();
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }   
-   
 	public List<User> getAllUsers() {
+		List<User> userList = null;
+
+		userList = new ArrayList<User>();
+
+		return userList;
+	}
+	
+	public boolean checkAccount(String username,String password)
+	{
+		boolean valid = em.checkAccount(username, password);
+		return valid;
+	}
+
+	private void saveUserList(List<User> userList) {
+		try {
+			File file = new File("Users.dat");
+			FileOutputStream fos;
+
+			fos = new FileOutputStream(file);
+
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(userList);
+			oos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public User getKorisnik(String id) {
+		User r = new User();
 		return r;
 	}
 
