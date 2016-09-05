50240a8b42f23a13718ed7fec7650c9e1d61d8a3
dani924 <sekulic07@gmail.com>
2016-09-05 22:43:49 +0200

    Implementirano da se pri usvajanju zakona on automatski arhivira. Dodate metode za eksportovanje u pdf i xhtml zakona ciji je id prosledjen.

--- xml/XML/src/src/services/ExportService.java
+++ xml/XML/src/src/services/ExportService.java
@@ -15,6 +15,7 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBContext;
@@ -38,6 +39,7 @@ import org.xml.sax.SAXException;
 
 import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
 
+import src.database.EntityManagerMarkLogic;
 import src.rs.gov.parlament.propisi.Zakon;
 import src.util.ConnUtil;
 
@@ -139,10 +141,139 @@ public class ExportService {
 		return "Zakon je eksportovan u PDF";
 	}
 	
+	@Path("/pdf/{id}")
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public String exportAsPDF(@PathParam("id") String id) throws SAXException, IOException, JAXBException {
+		
+		EntityManagerMarkLogic<Zakon, Long> em = new EntityManagerMarkLogic<Zakon, Long>("", "src.rs.gov.parlament.propisi");
+		Zakon z = em.getZakonByID(id);
+				
+		fopFactory = FopFactory.newInstance(getFile("fop.xconf"));
+		transformerFactory = new TransformerFactoryImpl();
+		// Point to the XSL-FO file
+		File xsltFile = null;
+		try {
+			xsltFile = getFile("zakon_template_pdf.xsl");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Create transformation source
+		StreamSource transformSource = new StreamSource(xsltFile);
+
+		File tmpdir = (File)servletContext.getAttribute("javax.servlet.context.tempdir");
+	    File output = new File(tmpdir, z.getID() + "toPDF.xml");
+	    
+	    JAXBContext context;
+		try {
+			context = JAXBContext.newInstance("src.rs.gov.parlament.propisi");
+			// Marshaller je objekat zadužen za konverziju iz objektnog u XML model
+	     	Marshaller marshaller = context.createMarshaller();
+		 		
+		 	// Podešavanje marshaller-a
+		 	marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		 		
+		 	// Umesto System.out-a, može se koristiti FileOutputStream
+		 	marshaller.marshal(z, output);
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		// Initialize the transformation subject
+		StreamSource source = new StreamSource(output);
+
+		// Initialize user agent needed for the transformation
+		FOUserAgent userAgent = fopFactory.newFOUserAgent();
+
+		// Create the output stream to store the results
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+		// Initialize the XSL-FO transformer object
+		Transformer xslFoTransformer;
+		try {
+			xslFoTransformer = transformerFactory
+					.newTransformer(transformSource);
+			// Construct FOP instance with desired output format
+			Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, userAgent,
+					outStream);
+
+			// Resulting SAX events
+			Result res = new SAXResult(fop.getDefaultHandler());
+
+			// Start XSLT transformation and FOP processing
+			xslFoTransformer.transform(source, res);
+		} catch (FOPException | TransformerException e) {
+			e.printStackTrace();
+		}
+		
+		// Generate PDF file
+		File pdfFile = new File(tmpdir, z.getID() + ".pdf");
+
+		OutputStream out;
+		try {
+			out = new BufferedOutputStream(new FileOutputStream(
+					pdfFile));
+			out.write(outStream.toByteArray());
+
+			System.out.println("[INFO] File \"" + pdfFile.getCanonicalPath()
+					+ "\" generated successfully.");
+			out.close();
+
+			System.out.println("[INFO] End.");
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		return "Zakon je eksportovan u PDF";
+	}
+	
 	@Path("/xhtml")
 	@POST
 	@Consumes(MediaType.APPLICATION_XML)
 	public String exportAsXHTML(Zakon z) throws SAXException, IOException {
+		
+		// Point to the XSL-FO file
+		File xsltFile = null;
+		try {
+			xsltFile = getFile("zakon_template_xhtml.xsl");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+		File tmpdir = (File)servletContext.getAttribute("javax.servlet.context.tempdir");
+	    File output = new File(tmpdir, z.getID() + "XHTML.xml");
+	    
+	    JAXBContext context;
+		try {
+			context = JAXBContext.newInstance("src.rs.gov.parlament.propisi");
+			// Marshaller je objekat zadužen za konverziju iz objektnog u XML model
+	     	Marshaller marshaller = context.createMarshaller();
+		 		
+		 	// Podešavanje marshaller-a
+		 	marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		 	
+		 	marshaller.setProperty("com.sun.xml.internal.bind.xmlHeaders", 
+		 		    "<?xml-stylesheet type=\"text/xsl\" href=\"zakon_template_xhtml.xsl\" ?>");
+		 	
+		 	// Umesto System.out-a, mode se koristiti FileOutputStream
+		 	marshaller.marshal(z, output);
+			
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		return "Zakon je eksportovan u XHTML";
+	}
+	
+	@Path("/xhtml/{id}")
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public String exportAsXHTML(@PathParam("id") String id) throws SAXException, IOException, JAXBException {
+		
+		EntityManagerMarkLogic<Zakon, Long> em = new EntityManagerMarkLogic<Zakon, Long>("", "src.rs.gov.parlament.propisi");
+		Zakon z = em.getZakonByID(id);
 		
 		// Point to the XSL-FO file
 		File xsltFile = null;
--- xml/XML/src/src/services/ResourceService.java
+++ xml/XML/src/src/services/ResourceService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -114,9 +115,9 @@ public class ResourceService {
 	}
 	
 	@GET
-	@Path("/zakonij")
+	@Path("/zakonj/{i}")
     @Produces(MediaType.APPLICATION_JSON)
-	public Zakon getZakonByID(String id) {
+	public Zakon getZakonByID(@PathParam("i")String id) {
 		resource = new ResourceDao("src.rs.gov.parlament.propisi");
 		Zakon zakon = resource.getZakonByID(id);
 		return zakon;
